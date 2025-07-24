222c1271f2b817df72b6f152e3955adc82e8ce1a
Deepak-Dhinwa <ddhinwa05@gmail.com>
2025-07-24 19:08:48 +0530

    dn

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../common/footer/footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,12 @@ import { FooterComponent } from '../common/footer/footer';
 })
 export class App {
   protected title = 'seedstars';
+
+  ngOnInit() {
+    AOS.init({
+      duration: 900,
+      once: true,
+      easing: 'ease-in-out',
+    });
+  }
 }
